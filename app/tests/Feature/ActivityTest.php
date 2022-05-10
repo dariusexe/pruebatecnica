e0da4c0be27eb6f19365e8d05ad908533efc0027
Dario Vallejo Carrasco <darioinfor@gmail.com>
2022-05-10 05:35:53 +0200

    some new tests

--- app/tests/Feature/ActivityTest.php
+++ app/tests/Feature/ActivityTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Enum\UserRole;
 use App\Models\Activity;
 use App\Models\Project;
 use Database\Factories\ActivityFactory;
@@ -11,6 +12,7 @@ use Tests\TestCase;
 
 class ActivityTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic feature test example.
      *
@@ -18,12 +20,49 @@ class ActivityTest extends TestCase
      */
     public function test_show_all_activites_from_user_and_project()
     {
-        $activity = Activity::factory()->hasAttached($this->loggedInUser, ['role_id' => 1]);
+        $activity_factory = Activity::factory()->hasAttached($this->loggedInUser, ['role_id' => UserRole::MANAGER]);
 
-        $project = Project::factory()->has($activity)->create();
+        $project = Project::factory()->has($activity_factory)->create();
+
+        $activity = $project->activities()->first();
 
         $response = $this->getJson('api/project/' . $project->id . '/activities');
         $response->assertStatus(200)
-        ->assertJsonFragment(['id' => $project->activities()->get()[0]->id]);
+        ->assertJsonFragment(['id' => $activity->id]);
+    }
+    
+
+    public function test_not_found_activity_when_scope_binding_fails(){
+        $activity_factory = Activity::factory();
+        $project = Project::factory()->has($activity_factory)->create();
+        $activity = $project->activities()->first();
+        $response = $this->getJson('api/project/' . $project->id + 1 . '/activities/'.$activity->id);
+        $response->assertStatus(404);
+    }
+
+    public function test_create_activity(){
+        $project = Project::factory()->hasAttached($this->loggedInUser, ['role_id' => UserRole::MANAGER])->create();
+
+        $data = [
+            'name' => 'test project'
+        ];
+
+        $response = $this->postJson('api/project/'.$project->id. '/activities' , $data);
+
+        $response->assertStatus(201)
+        ->assertJsonFragment($data);
+    }
+    public function test_user_only_can_be_manager_or_participant(){
+        $activity_factory = Activity::factory()->hasAttached($this->loggedInUser, ['role_id' => UserRole::MANAGER]);
+        $project = Project::factory()->has($activity_factory)->hasAttached($this->loggedInUser, ['role_id' => UserRole::MANAGER])->create();
+        $activity = $project->activities()->first();
+        $data = [
+            'user_id'=> $this->loggedInUser->id,
+            'role_id'=> UserRole::PARTICIPANT
+        ];
+        $response = $this->postJson('api/projects/'.$project->id.'/activities/'.$activity->id.'/users', $data);
+        $response->assertStatus(400)
+        ->assertJson(['error' => 'User already has been added']);
+
     }
 }
